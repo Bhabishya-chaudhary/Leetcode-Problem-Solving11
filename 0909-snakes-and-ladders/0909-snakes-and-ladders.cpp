5eb6be245dfc7c8810717b5f91150b8908ec1659
Bhabishya <chaudharybhabishya3@gmail.com>
2026-07-16 21:24:37 +0545

    Time: 0 ms (100%), Space: 17 MB (60.53%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,63 +1,70 @@
 class Solution {
 public:
     int n;
+
+   pair<int, int> getcoord(int num){
+    int RT = (num-1)/n;
+    int RB = (n-1)-RT;
+
+    int col = (num-1)%n;
+
+    if((n%2==1 && RB%2==1 )||  (n%2==0 && RB%2==0))
+      col = (n-1)-col;
+      return make_pair(RB, col);
+   }
+
+
     
-    pair<int, int> getCoord(int s) {
-        int row = n-1-(s-1)/n;
-        
-        int col = (s-1)%n;
-        
-        if((n%2==1 && row%2==1)||(n%2==0 && row%2==0))
-            col = n-1-col;
-        
-        return make_pair(row, col);
-    }
     
     int snakesAndLadders(vector<vector<int>>& board) {
-        n = board.size();
-        
+
+         n = board.size();
         int steps = 0;
+
         queue<int> que;
-        
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        visited[n-1][0] = true;
-        
         que.push(1);
-        vector<bool> seen(n*n+1,false);
-        
-        
-        while(!que.empty()) {
-            
-            int N = que.size();
-            while(N--) {
-            
-                int x = que.front();
-                que.pop();
 
-                if(x == n*n)
-                    return steps;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        //initally
+        // F F F F F F
+        // F F F F F F
+        // F F F F F F
+        // F F F F F F
+        // F F F F F F
+        // F F F F F F
 
-                for(int k = 1; k<=6; k++) {
-                    if(x+k > n*n)
-                        break;
+        visited[n-1][0]=true;
 
-                    pair<int, int> coord = getCoord(x+k);
-                    int r = coord.first;
-                    int c = coord.second;
-                    if(visited[r][c] == true)
-                        continue;
+        while(!que.empty()){
+          int N = que.size();
 
-                    visited[r][c] = true;
-                    if(board[r][c] == -1)
-                        que.push(k+x);
-                    else {
-                        que.push(board[r][c]);
-                    }
-                }
+          while(N--){
+            int x = que.front();
+            que.pop();
+
+            if (x==n*n)
+            return steps;
+
+            for(int k=1; k<=6; k++){
+              int value = x+k;
+
+              if(x+k>n*n)
+              break;
+
+              pair<int, int > coord = getcoord(value);
+              int r = coord.first;
+              int c = coord.second;
+              if(visited[r][c]) 
+              continue;
+              visited[r][c]=true;
+              if(board[r][c]==-1)
+                que.push(value);
+              else
+                que.push(board[r][c]);
             }
-            steps++;
+          }
+          steps++;
         }
-        
         return -1;
     }
 };
